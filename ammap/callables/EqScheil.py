8be37fc6ec20f9e93b53796e7fc4bfce396b3f02
amr8004 <90706046+amr8004@users.noreply.github.com>
2024-07-24 21:24:53 +0000

    include elemental components of phase fraction from scheil for present phases

--- ammap/callables/EqScheil.py
+++ ammap/callables/EqScheil.py
@@ -1,6 +1,7 @@
 from pycalphad import Database, equilibrium, variables as v
 from pycalphad.core.utils import instantiate_models, filter_phases, unpack_components
 from pycalphad.codegen.callables import build_phase_records
+import pandas as pd
 import math
 
 # Problem Specific setup for our 9-element space exploration. Make sure that the elements 
@@ -70,6 +71,20 @@ def scheil_callable(elP):
     Sfrac = sol_res.fraction_solid
     scheilT = sol_res.temperatures
     solT = sol_res.temperatures[len(scheilT) - 1]
+    ddict = sol_res.x_phases
+    keys_to_remove_from_ddict = []
+    for ddict_key, dict_value in ddict.items():
+        keys_to_remove_from_dict = [key for key, value in dict_value.items() if pd.isna(value).all()]
+    # Remove marked keys from the dictionary
+        for key in keys_to_remove_from_dict:
+            del dict_value[key]   
+    # If the dictionary is empty, mark the key in the defaultdict for removal
+        if not dict_value:
+            keys_to_remove_from_ddict.append(ddict_key)
+# Remove the marked keys from the defaultdict
+    for key in keys_to_remove_from_ddict:
+        del ddict[key]
+    yPhase = sol_res.Y_phases
     for i, value in enumerate(Lfrac):
         if value < 1:
             break
@@ -82,6 +97,8 @@ def scheil_callable(elP):
         'solT': solT,
         'liqT': liqT,
         'phaseFractions': test,
+        'xPhase': ddict#,
+        #'yPhase': yPhase
     }
 
 if __name__ == "main":
