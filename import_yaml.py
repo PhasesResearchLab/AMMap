67a4b4a1c58a3d7be93fc69ba86f6a8ccf367adb
amr8004 <90706046+amr8004@users.noreply.github.com>
2025-09-26 14:38:00 +0000

    Housekeep 0.4

--- import_yaml.py
+++ import_yaml.py
@@ -1,8 +1,13 @@
 import yaml
 import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Process a YAML file.')  
+parser.add_argument('filename', type=str, help='Path to the input YAML file')  
+args = parser.parse_args() 
 
 # Load the YAML file
-with open('example_input.yaml', 'r') as file:
+with open(args.filename, 'r') as file:
     yaml_content = yaml.safe_load(file)
 
 # Extract the designSpaces and elementalSpaces keys
@@ -10,10 +15,15 @@ design_spaces = yaml_content['designSpaces']
 elemental_spaces = yaml_content['elementalSpaces']
 
 # Combine all the names of each entry in designSpaces
-attainableSpaceComponents = []
-for entry in design_spaces:
-    components = re.findall(r'[A-Z][a-z]*', entry['name'])
-    attainableSpaceComponents.extend(components)
+# attainableSpaceComponents = []
+# for entry in design_spaces:
+#     components = re.findall(r'[A-Z][a-z]*', entry['name'])
+#     attainableSpaceComponents.extend(components)
+attainableSpaceComponents = [
+    component
+    for entry in design_spaces
+    for component in re.findall(r'[A-Z][a-z]*', entry['name'])
+]
 
 # Remove duplicates and sort the list
 attainableSpaceComponents = sorted(set(attainableSpaceComponents))
